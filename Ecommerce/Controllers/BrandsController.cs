b74be4581f265444d91cde0d4d928111332cfb51
Alkhanoff <aalxanov@std.qu.edu.az>
2018-10-11 16:48:58 +0400

    Add project files.

---
+++ Ecommerce/Controllers/BrandsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ecommerce.Models;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Ecommerce.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private ecommerceEntities db = new ecommerceEntities();
+
+        // GET: Brands
+        public ActionResult Index()
+        {
+            return View(db.Brands.ToList());
+        }
+
+        // GET: Brands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Brands/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+
+        public ActionResult Create(Brand brand , HttpPostedFileBase image)
+        {
+            var myAccount = new Account { ApiKey = "826331127339442", ApiSecret = "8SQi6Vti80ZwES3LeeBDFnSqBWQ", Cloud = "dycqowxvx" };
+            Cloudinary _cloudinary = new Cloudinary(myAccount);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(image.FileName , image.InputStream)
+            };
+        var uploadResult = _cloudinary.Upload(uploadParams);
+            brand.image = uploadResult.SecureUri.AbsoluteUri;
+            brand.status = true;
+            if (ModelState.IsValid)
+            {
+
+                db.Brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brand);
+        }
+
+        // GET: Brands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brands/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,image,status")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        // GET: Brands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Brand brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Brand brand = db.Brands.Find(id);
+            db.Brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
